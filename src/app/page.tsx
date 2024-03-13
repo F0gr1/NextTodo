77cbf9d77fc44d59a98975882ca40729ef8d2027
F0gr1 <ishigami0314@icloud.com>
2024-03-13 11:30:17 +0900

    update style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <main className={styles.main}>
+      <h1> Todo </h1>
       <TodoList />
     </main>
   );
