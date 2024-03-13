77bf3febf42a6a988f8a6359c67895750cc24c80
F0gr1 <ishigami0314@icloud.com>
2024-03-13 11:28:00 +0900

    update task

--- src/app/Components/TodoList.tsx
+++ src/app/Components/TodoList.tsx
@@ -1,29 +1,58 @@
 "use client";
 import React, { useState } from "react";
-
+import style from "./todo.module.css";
+type task = {
+  id: number;
+  title: string;
+  isDone: boolean;
+};
 const TodoList = () => {
-  const [tasks, setTasks] = useState(["task1", "task2", "task3"]);
-  const [task, setTask] = useState("");
-  const addTask = () => {
-    setTasks([...tasks, task]);
+  const [tasks, setTasks] = useState<task[]>([]);
+  const [task, setTask] = useState<task>({ id: 1, title: "", isDone: false });
+  const generateId = () => {
+    const newId = tasks.length > 0 ? tasks[tasks.length - 1].id + 1 : 1;
+    return newId;
   };
 
+  const setTaskWithAutoIncrement = (title: string) => {
+    setTask({ id: generateId(), title: title, isDone: false });
+  };
+  const addTask = () => {
+    setTasks([...tasks, task]);
+    setTask({ id: generateId(), title: "", isDone: false });
+  };
+  const DoneTask = (index: number) => {
+    const newTasks = [...tasks];
+    newTasks[index].isDone = !newTasks[index].isDone;
+    setTasks(newTasks);
+  };
   return (
-    <div>
-      <ul>
+    <div className={style.container}>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          addTask();
+        }}
+      >
+        <input
+          type="text"
+          value={task.title}
+          onChange={(e) => setTaskWithAutoIncrement(e.target.value)}
+          placeholder="ADD TODO"
+          className={style.inputTodo}
+        />
+      </form>
+      <ul className={style.todos}>
         {tasks.map((task, index) => (
-          <li key={index}>
-            <input type="checkbox" />
-            {task}
+          <li
+            key={index}
+            className={style.todo}
+            onClick={() => DoneTask(index)}
+          >
+            {!task.isDone ? task.title : <s>{task.title}</s>}
           </li>
         ))}
       </ul>
-      <input
-        type="text"
-        value={task}
-        onChange={(e) => setTask(e.target.value)}
-      />
-      <button onClick={addTask}>Add Task</button>
     </div>
   );
 };
